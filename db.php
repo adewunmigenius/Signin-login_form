28f1b0d7ecd1b0acdbff035e9a2ec16937c6d282
OJO Ahmed Adewunmi <adewunmigenius@gmail.com>
2017-08-02 18:31:19 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn =mysqli_connect("localhost","root","genius10","logintest");
+$conn =mysqli_connect("$db_host","username","password","$db_name");
 if(!$conn){
 	die("Connection failed:".mysqli_connect_error());
 }
