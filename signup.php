0975880a7257079078dbaf487c0d216af4359432
OJO Ahmed Adewunmi <adewunmigenius@gmail.com>
2017-08-09 12:42:54 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,8 +7,12 @@ $last = $_POST['last'];
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
+$salt1 = "qm&h*";
+$salt2 = "pg!@";
+$prtpwd = hash('ripemd128', "$salt1$pwd$salt2");
+
 $sql ="INSERT INTO user(first,last,uid,pwd)
-VALUES ('$first', '$last', '$uid', '$pwd')";
+VALUES ('$first', '$last', '$uid', '$prtpwd')";
 $result = $conn->query($sql);
 ?>
 
